dd63236e8c38f85a1517a4d4711e092698b8fd8a
chipcHE163829 <chipche163829@fpt.edu.vn>
2024-08-07 00:03:18 +0700

    [Create BookingController]
    Create and code bookingcontroller

---
+++ SWP391.E.BL5.G3/SWP391.E.BL5.G3/Controllers/BookingController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using SWP391.E.BL5.G3.Models;
+
+namespace SWP391.E.BL5.G3.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly traveltestContext _context;
+
+        public BookingController(traveltestContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> ListTour(string searchString)
+        {
+            var tours = from t in _context.Tours
+                        select t;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                tours = tours.Where(s => s.Name.Contains(searchString));
+            }
+
+            return View(await tours.ToListAsync());
+        }
+    }
+}
