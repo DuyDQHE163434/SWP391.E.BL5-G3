caccb5dba36f03b6aec20c9cfe57b76d3acd6eaf
DuyDQHE163434 <133103039+DuyDQHE163434@users.noreply.github.com>
2024-08-06 20:59:04 +0700

    Create and View Login

---
+++ SWP391.E.BL5.G3/SWP391.E.BL5.G3/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SWP391.E.BL5.G3.Models;
+
+namespace SWP391.E.BL5.G3.Controllers
+{
+    public class LoginController : Controller
+    {
+        traveltestContext context = new traveltestContext();
+        public IActionResult Login(int mess)
+        {
+            if (mess == 1)
+            {
+                ViewBag.mess1 = "Thông tin tài khoản không tồn tại , kiểm tra lại thông tin đăng nhập";
+            }
+            else if (mess == 2)
+            {
+                ViewBag.mess1 = "Vui lòng đăng nhập trước khi thao tác";
+            }
+            else if (mess == 3)
+            {
+                ViewBag.mess1 = "This email has been registered !!!";
+            }
+            else if (mess == 4)
+            {
+                ViewBag.mess1 = "Account registration successful !!!";
+            }
+            else
+            {
+                ViewBag.mess1 = "";
+            }
+            return View();
+        }
+    }
+}
