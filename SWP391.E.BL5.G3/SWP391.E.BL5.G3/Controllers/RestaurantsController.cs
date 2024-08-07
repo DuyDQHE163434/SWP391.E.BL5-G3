1d023534f6f80161256670c12fb46c077ac54e62
ANHNT-LAPTOP\NTA-PC <anhnguyen.tuan0218@gmail.com>
2024-08-07 09:20:13 +0700

    Add pagination to RestaurantsController

--- SWP391.E.BL5.G3/SWP391.E.BL5.G3/Controllers/RestaurantsController.cs
+++ SWP391.E.BL5.G3/SWP391.E.BL5.G3/Controllers/RestaurantsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SWP391.E.BL5.G3.Models;
+using X.PagedList;
 
 namespace SWP391.E.BL5.G3.Controllers
 {
@@ -12,9 +13,16 @@ namespace SWP391.E.BL5.G3.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            return View(_context.Restaurants.ToList());
+            return View(DisplayPagedList(page, 5));
+        }
+
+        public IPagedList<Restaurant> DisplayPagedList(int pageNumber, int pageSize)
+        {
+            pageNumber = pageNumber < 1 ? 1 : pageNumber;
+            var restaurants = _context.Restaurants.ToPagedList(pageNumber, pageSize);
+            return restaurants;
         }
 
     }
