1d823ea0b89730b386c06a8da40cef0eca9f63f0
DuyDQHE163434 <133103039+DuyDQHE163434@users.noreply.github.com>
2024-08-08 20:37:20 +0700

    Commit access Login

---
+++ SWP391.E.BL5.G3/SWP391.E.BL5.G3/DAO_Context/DAO.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+using System.Net.Mail;
+using System.Reflection;
+using System.Security.Principal;
+using System.Text;
+using System.Text.RegularExpressions;
+using SWP391.E.BL5.G3.Models;
+
+
+
+namespace SWP391.E.BL5.G3.DAO_Context
+{
+    public class DAO
+    {
+        traveltestContext context = new traveltestContext();
+
+        public User Login(string userName, string pass)
+        {
+            try
+            {
+                User account = context.Users.Where(x => x.Email.Trim().ToLower().Equals(userName.Trim().ToLower()) == true && x.Password.Trim().ToLower().Equals(pass.Trim().ToLower()) == true).FirstOrDefault();
+                if (account != null)
+                {
+                    return account;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+
+
+
+
+    }
+}
