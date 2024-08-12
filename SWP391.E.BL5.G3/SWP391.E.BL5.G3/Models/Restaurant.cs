2d877fe7b21d1ce83c7da4349179ccfcf86e928f
chipcHE163829 <chipche163829@fpt.edu.vn>
2024-08-12 22:24:29 +0700

    [update model]
    update

--- SWP391.E.BL5.G3/SWP391.E.BL5.G3/Models/Restaurant.cs
+++ SWP391.E.BL5.G3/SWP391.E.BL5.G3/Models/Restaurant.cs
@@ -17,10 +17,10 @@ namespace SWP391.E.BL5.G3.Models
         public string Description { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
-        public string Image { get; set; }
+        public string? Image { get; set; }
         public int? ProvinceId { get; set; }
         public string CuisineType { get; set; }
-        public string ContactNumber { get; set; }
+        public string? ContactNumber { get; set; }
 
         public virtual ICollection<Booking> Bookings { get; set; }
         public virtual ICollection<Tour> Tours { get; set; }
--- SWP391.E.BL5.G3/SWP391.E.BL5.G3/Models/User.cs
+++ SWP391.E.BL5.G3/SWP391.E.BL5.G3/Models/User.cs
@@ -14,13 +14,13 @@ namespace SWP391.E.BL5.G3.Models
         public int UserId { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Image { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? Image { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PhoneNumber { get; set; }
         public int? RoleId { get; set; }
         public bool? Action { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public bool? Gender { get; set; }
 
         public virtual ICollection<Booking> Bookings { get; set; }
