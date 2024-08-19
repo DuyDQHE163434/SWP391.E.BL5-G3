9ad27d4cffdfe15e106233bdf7650d23b6a6d8fa
ANHNT-LAPTOP\NTA-PC <anhnthe161811@fpt.edu.vn>
2024-08-19 11:22:09 +0700

    Create List Vehicles View

--- SWP391.E.BL5.G3/SWP391.E.BL5.G3/Controllers/VehiclesController.cs
+++ SWP391.E.BL5.G3/SWP391.E.BL5.G3/Controllers/VehiclesController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SWP391.E.BL5.G3.Models;
 
 namespace SWP391.E.BL5.G3.Controllers
 {
     public class VehiclesController : Controller
     {
-        public IActionResult Index()
+        private readonly traveltestContext _context;
+
+        public VehiclesController(traveltestContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult ListVehicles()
+        {
+            var vehicles = _context.Vehicles.Include(item => item.Province);
+            return View(vehicles.ToList());
         }
     }
 }
